309b5692eff1a26592b2b763673f8fbda720cfee
Osmin <osminv91@gmail.com>
2022-12-11 23:01:11 -0500

    app with react

---
+++ src/restaurantes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Header1} from './script'
+import {Section3} from './script'
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<div>
+    <div>
+        <Header1/>
+    </div>
+    <div>
+        <Section3 />
+    </div>
+</div>)
--- src/script.js
+++ src/script.js
@@ -11,6 +11,7 @@ function Header1(){
                                 <li><a href="busqueda.html">Buscar</a></li>
                                 <li><a href="restaurantes.html">Restaurantes</a></li>
                                 <li><a href="nuevoResturante.html" >Registrar restaura</a></li>
+                                <li><a href="index.html" >Inicion</a></li>
                          </ul>
                     </div>
                 </nav>
@@ -29,4 +30,38 @@ function Section1 (){
         </p>
     </picture>
 </section>)
+}
+
+function Section2 (){
+    return(<section>
+        <div class="div-class">
+            <form action="Controlador" method="POST">
+                <label class="label" for="nombre">Nombre</label><br></br>
+                <input class="campo" type="text" name="nombre" placeholder="nombre" /><br></br>
+                <button class="button" type="submit" action="buscar" value="averiguar" >Buscar</button><br></br>
+            </form>
+        </div>
+    </section>)
+}
+
+function Section3 (){
+    const restaurantes = [["img/La_cuchara.png","La cuchara","El mejor restaurante en comida tradicional"]
+                     ,["img/La_montaña.png","La montaña","Ofrecemos platos de muy buen sador"]
+                     ,["img/La_sopa.png","La sopa","Las mejores platos de colombia, estamos especializados en la elaboracion de sopas tradicionales de colombia"]]
+    
+    let max= restaurantes.length -1
+    let min=0
+    let maxfor= restaurantes.length
+    let valor= true
+    if(valor){
+        document.write("<div id=\"\" class=\"div-class\" >"+"<section>")
+        for(let i=0; i<maxfor;i++){
+            const arreglo= restaurantes[i]
+            document.write("<div class=\"div-section\">"+"<img src=\""+arreglo[0]+"\" width=\"100\" heigth=\"100\" alt=\"\" ><br>"+"<h3>"+arreglo[1]+"</h3><br>"+"<p>"+arreglo[2]+"</p>"+"</div>")
+            if(i==max){
+                valor=false
+            }
+        }
+        document.write("</div>"+"</section>")
+    }
 }
