693a60163a67c2f07938dd8caf386c5ad913dfa9
Osmin <osminv91@gmail.com>
2022-12-11 22:28:41 -0500

    Apricaciones...

---
+++ src/script.js
@@ -0,0 +1,32 @@
+function Header1(){
+    return(<header header class="div-margin">
+            <div>
+                <img src="/public/Ubicacion.png" width="100" height="100" alt="Logo de empresa" />
+                <h1>Restaurantes</h1>
+            </div>
+            <div>
+                <nav>
+                    <div class="nav-main">
+                        <ul class="menu">
+                                <li><a href="busqueda.html">Buscar</a></li>
+                                <li><a href="restaurantes.html">Restaurantes</a></li>
+                                <li><a href="nuevoResturante.html" >Registrar restaura</a></li>
+                         </ul>
+                    </div>
+                </nav>
+            </div>
+    </header>)
+}
+
+function Section1 (){
+    return(<section id="section" class="div-margin">"
+    <picture id="the-picture">
+        <img class="the-img" src="/pubblic/En-todo-mundo.png" width="500" height="300" alt="Logo de Presentacion" />
+        <img class="the-img" src="/public/Trato.png" width="500" height="300" alt="Logo de Presentacion" />
+        <p>Somos un puente que conecta personas con las mejores experiencias gastronomicas<br>
+        <strong>estamos felices de brindarle  los mejores restaurantes,</strong></br>
+        cada dia nos procuramos de ofrecer las mejores experiencias
+        </p>
+    </picture>
+</section>)
+}
